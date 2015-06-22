58c6244ce68717b4d118b4c5997b28ab28cfdd2f
Alexander Malikov <schn27@gmail.com>
2015-06-23 02:13:14 +0400

    fix launch script

--- _.cmd
+++ _.cmd
@@ -1,32 +1,47 @@
 @echo off
 set JARNAME=kgainfobot-1.0.jar
+
 set LOGIN=H6Pu9bp
 set PASS=NawVUVi
-set TIMEOUT=30
-set ENDTIME=10:05:00.00
+
+set TIMEOUT=1
+set ENDTIME="10:30"
 
 set TASK1=0
+set T1STRUCT=812000
+set T1DEPART=812003
+set T1THEME=11
+set T1TIME=11:30
+set T1CMNT="..."
+
 set TASK2=0
+set T2STRUCT=812000
+set T2DEPART=900001
+set T2THEME=12
+set T2TIME=11:00
+set T2CMNT="..."
 
 :loop
 
 if %TASK1% neq 0 goto task2
 
-echo attempt to register in 812003
-java -jar %JARNAME% login=%LOGIN% pass=%PASS% cmd=register structure=812000 department=812003 theme=11 time=11:30 comment="..." timeout=%TIMEOUT%
+echo attempt to register in %T1DEPART%
+java -jar %JARNAME% login=%LOGIN% pass=%PASS% cmd=register structure=%T1STRUCT% department=%T1DEPART% theme=%T1THEME% time=%T1TIME% comment=%T1CMNT% timeout=%TIMEOUT%
 if %ERRORLEVEL% equ 0 set TASK1=1
 
 :task2
 
 if %TASK2% neq 0 goto check
 
-echo attempt to register in 900001
-java -jar %JARNAME% login=%LOGIN% pass=%PASS% cmd=register structure=812000 department=900001 theme=12 time=11:00 comment="..." timeout=%TIMEOUT%
+echo attempt to register in %T2DEPART%
+java -jar %JARNAME% login=%LOGIN% pass=%PASS% cmd=register structure=%T2STRUCT% department=%T2DEPART% theme=%T2THEME% time=%T2TIME% comment=%T2CMNT% timeout=%TIMEOUT%
 if %ERRORLEVEL% equ 0 set TASK2=1
 
 :check
 
-rem if %TIME% gtr %ENDTIME% goto end
+set T=%TIME:~0,5%
+
+if "%T%" geq %ENDTIME% goto end
 if %TASK1% equ 0 goto loop
 if %TASK2% equ 0 goto loop
 
